05d7293ab6429b613f5674e1f2782620fdec343c
panjingping <panjingping@gmail.com>
2020-06-09 11:50:40 +0800

    add user.go

--- router/router.go
+++ router/router.go
@@ -1,14 +1,23 @@
 package router
+
 import (
 	"backend_template/controller"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
+func DummyMiddleware(c *gin.Context) {
+	fmt.Println("Im a dummy!")
+
+	// Pass on to the next-in-chain
+	c.Next()
+}
 func InitRouter() *gin.Engine {
 
 	PersonController := controller.PersonController{}
 	router := gin.Default()
+	router.Use(DummyMiddleware)
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/persons", PersonController.FindAll)
@@ -18,10 +27,7 @@ func InitRouter() *gin.Engine {
 		v1.PUT("/persons/:id", PersonController.Update)
 		v1.DELETE("/persons/:id", PersonController.Delete)
 
-
-
 	}
-
 
 	return router
 }
