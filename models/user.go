ef4c455211d2a8bb1827c1e4c243694ec6b44462
panjingping <panjingping@gmail.com>
2020-06-09 15:23:26 +0800

    create util package and add pagable.go

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type User struct {
+	gorm.Model
 	ID       uint   `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
