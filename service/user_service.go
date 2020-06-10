65f51a3f05d6d95ecbb77190d7e4cc4eef1de6fc
panjingping <panjingping@gmail.com>
2020-06-10 10:16:13 +0800

    UPDATE

---
+++ service/user_service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"backend_template/models"
+	"backend_template/repository"
+)
+
+type UserService struct {
+	BaseService
+	repository.UserRepository
+}
+
+// NewPersonService PersonService的构造函数
+func NewUserService(p repository.UserRepository) UserService {
+	return UserService{UserRepository: p}
+}
+
+//FindAll Fetch all person data
+
+func (s *UserService) FindAll() []models.User {
+	var users []models.User
+	users = s.UserRepository.FindAll()
+
+	return users
+}
+
+func (p *UserService) FindByID(id uint) models.User {
+	return p.UserRepository.FindByID(id)
+}
+func (p *UserService) Save(user models.User) models.User {
+	p.UserRepository.Save(user)
+
+	return user
+}
+
+func (p *UserService) Delete(user models.User) {
+	p.UserRepository.Delete(user)
+}
