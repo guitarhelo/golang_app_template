ef28e87f6f1b6dffc96da151b9b1d117120d2229
panjingping <panjingping@gmail.com>
2020-06-11 12:53:42 +0800

    add rbac.go

--- middleware/rbac.go
+++ middleware/rbac.go
@@ -11,14 +11,31 @@ type Rbac struct {
 	service.UserService
 }
 
+var userDto dto.UserDTO
+
+func GetUserPermissionList() []dto.ResourceDTO {
+	var userPermissionList []dto.ResourceDTO
+	permissionList := userDto.Resources
+
+	for _, v := range permissionList {
+		//fmt.Println(k, v)
+		for i := 0; i < len(v); i++ {
+			userPermissionList = append(userPermissionList, v[i])
+		}
+	}
+	fmt.Print(userPermissionList)
+
+	return userPermissionList
+}
 func UserPermissionMiddleware(c *gin.Context) {
-	var user_dto dto.UserDTO
 	UserService := service.UserService{}
 	fmt.Println("Im a Rbac middleware!")
-	user_dto = UserService.LoadByName("john")
+	userDto = UserService.LoadByName("john")
 	url := c.Request.RequestURI
 	fmt.Print(url)
-	fmt.Print(user_dto)
+	fmt.Print(userDto)
+	userPermissionList := GetUserPermissionList()
+	fmt.Print(userPermissionList)
 	// Pass on to the next-in-chain
 	c.Next()
 }
