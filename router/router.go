11ea5c5d0c2287c53d239cc6e92e1ba2ae85e092
panjingping <panjingping@gmail.com>
2020-06-11 15:25:56 +0800

    add rbac.go

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"backend_template/controller"
 	"backend_template/middleware"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func InitRouter() *gin.Engine {
@@ -11,6 +12,19 @@ func InitRouter() *gin.Engine {
 	PersonController := controller.PersonController{}
 	router := gin.Default()
 	router.Use(middleware.UserPermissionMiddleware)
+	router.LoadHTMLGlob("templates/**/*.html")
+	router.LoadHTMLGlob("templates/**/*.html")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	router.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+	admin := router.Group("/admin")
+	admin.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "admin-overview.html", nil)
+	})
+
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/persons", PersonController.FindAll)
